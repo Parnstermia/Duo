f31b1ae54faaef19168e944be9fdeb4cbbaa9f87
@Parnstermia <Parnstermia@localhost.localdomain>
2017-02-03 11:24:21 +0100

    Fixed some Issues with the dictionary updater

--- update_dict.py
+++ update_dict.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python3
-def openDictionary(dictionary,txt):
-	with open(txt) as fil:
-		for row in fil:
-			words = row.split('\t')
-			dictionary[words[0].strip("\n")] = ""
-	return dictionary
-
-def includeWords(old_words_file ,old_dict ,newWords):
-	counter = 0;
-	with open(old_words_file,'a') as f:
-		with open(newWords) as new:
-			for word in new:
-				if word.strip("\n") not in old_dict:
-					old_dict[word] = ""
-					f.write(word)
-					counter = counter + 1
-
-	return counter			
-
-def updateDictionary(old_words_file, new_words_file ,dictionary):
-	dictionary = openDictionary(dictionary,old_words_file)
-	number = includeWords(old_words_file, dictionary, new_words_file)
-	print("There are <" + str(number) + "> new words registered")
+def openDictionary(word_list, txt):
+    with open(txt) as fil:
+        for row in fil:
+            words = row.split('\t')
+            word_list.append(words[0].strip("\n"))
+    return word_list
 
 
-Old_words = input("Enter the Old words dictionary      :\n")
-New_words = input("Enter the new words file text       :\n")
-dictionary = {'none': "default"}
+def includeWords(word_list, old_words_file, newWords):
+    counter = 0
+    with open(old_words_file, 'a') as f:
+        with open(newWords) as new:
+            for word in new:
+                aux = word.strip("\n")
+                if aux not in word_list:
+                    word_list.append(aux)
+                    f.write(aux)
+                    counter = counter + 1
 
-updateDictionary(Old_words,New_words,dictionary)
+    return counter
+
+
+def updateDictionary(word_list, old_words_file, new_words_file):
+    word_list = openDictionary(word_list, old_words_file)
+    number = includeWords(word_list, old_words_file, new_words_file)
+    print("There are <" + str(number) + "> new words registered")
+
+
+Old_words = input("Enter the Old words dictionary :\n------>")
+New_words = input("Enter the new words file text  :\n------>")
+word_list = []
+
+updateDictionary(word_list, Old_words, New_words)
