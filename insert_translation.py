2e8a2d27071b05ec55c748cb4b52518f82b07b47
@Parnstermia <Parnstermia@localhost.localdomain>
2017-02-03 12:46:48 +0100

    Added if __name__ = '__main__' for test/module purposes

--- insert_translation.py
+++ insert_translation.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python3
-#author     @Parnstermia
-#Project
-#brief  Simple script to insert new translation into our dictionary
+# author     @Parnstermia
+# Project
+# brief  Simple script to insert new translation into our dictionary
 #
-fi = input("Enter the dictionary to modify  :\n------>")
-fo = input("Enter the output dictionary     :\n------>")
-with open(fi, 'r+') as fi:
-    with open(fo, 'r+') as fo:
-        continuar = True
-        for line in fi:
-            if "\t" not in line :
-                if continuar:
-                    translation = input("Insert translation for <" +
-                                        line.strip("\n") + "> : ")
-                if translation == '0':
-                    continuar = False
-                    print("Translation stopped, wait until file is written.")
+
+
+def insertTranslations(fi, fo):
+    with open(fi, 'r+') as fi:
+        with open(fo, 'r+') as fo:
+            continuar = True
+            for line in fi:
+                if "\t" not in line:
+                    if continuar:
+                        translation = input("Insert translation for <" +
+                                            line.strip("\n") + "> : ")
+                    if translation == '0':
+                        continuar = False
+                        print("Translation stopped, wait until\
+                                file is written.")
+
+                    else:
+                        if not translation == "":
+                            line = line.strip("\n") + "\t" + translation + "\n"
+
+                    fo.write(line)
 
                 else:
-                    if not translation == "":
-                        line = line.strip("\n") + "\t" + translation + "\n"
+                    fo.write(line)
+        print(fo + " has been succesfully created.")
 
-                fo.write(line)
 
-            else:
-                fo.write(line)
-    print(fo + " has been succesfully created.")
+if __name__ = '__main__':
+    fi = input("Enter the dictionary to modify  :\n------>")
+    fo = input("Enter the output dictionary     :\n------>")
+    insertTranslations(fi, fo)
