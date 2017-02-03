96418becaf1aaf48592953fb4e288df8ccd90b39
@Parnstermia <Parnstermia@localhost.localdomain>
2017-02-03 11:22:30 +0100

    Committer: @Parnstermia <Parnstermia@localhost.localdomain>  Changes to be committed: 	new file:   insert_translation.py
     Changes not staged for commit:
    	modified:   update_dict.py

---
+++ insert_translation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#author     @Parnstermia
+#Project
+#brief  Simple script to insert new translation into our dictionary
+#
+fi = input("Enter the dictionary to modify  :\n------>")
+fo = input("Enter the output dictionary     :\n------>")
+with open(fi, 'r+') as fi:
+    with open(fo, 'r+') as fo:
+        continuar = True
+        for line in fi:
+            if "\t" not in line :
+                if continuar:
+                    translation = input("Insert translation for <" +
+                                        line.strip("\n") + "> : ")
+                if translation == '0':
+                    continuar = False
+
+                else:
+                    if not translation == "":
+                        line = line.strip("\n") + "\t" + translation + "\n"
+
+                fo.write(line)
+
+            else:
+                fo.write(line)
