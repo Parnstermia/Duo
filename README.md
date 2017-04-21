41bec4b93e62e465f77a8f855b6f5c248af1f1cf
Parnstermia <seloya@correo.ugr.es>
2017-04-21 22:03:45 +0200

    reup

--- README.md
+++
@@ -1,1 +0,0 @@
-You can add word-fetcher.js into a Tampermonkey script or use the console in the duolingo.com/words section to download your "own" dictionary.
---
+++ pruebas.py
@@ -0,0 +1,23 @@
+# This Python file uses the following encoding: utf-8
+from duolingo import Duolingo as duo
+
+##Pruebas prints
+#cadena = u's\xf8n'
+#nueva = cadena.encode('utf-8')
+#print(cadena.encode('utf-8'))
+#print(nueva)
+#print('måske')
+
+##Pruebas api
+#lingo = duo('parnstermia',password='guitarra')
+#lang = lingo.get_abbreviation_of("Danish")
+#vocab = lingo.get_vocabulary(language_abbr=lang)
+#know = lingo.get_known_words(lang)
+#print(vocab)
+#print(know)
+#print(len(know))
+
+
+#print('s\xf8n'.encode('utf-8'))
+#print(unicode('s\xf8n','utf-8'))
+#print(unicode('Von D\xc3\xbc', 'utf-8'))
